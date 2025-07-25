dd1d98687d279a2e0d9acdd8ec2f387effb26a53
MRAKS05 <sohonkhan86991@gmail.com>
2025-07-25 21:15:42 +0600

    Fixed the bug witht the topbar on the chapter page

--- react-vite-frontend/app/subjects/papers/physics_1st/ch-1.tsx
+++ react-vite-frontend/app/subjects/papers/physics_1st/ch-1.tsx
@@ -2,7 +2,7 @@
 
 import React, { useRef, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import TopBar from "../../../components/topbar-phy-1st";
+import TopBar from "../../../components/topbar";
 import NoInternetWarning from "../../../components/noInternetWarning";
 import { motion, AnimatePresence } from "framer-motion";
 import { App } from "@capacitor/app";
